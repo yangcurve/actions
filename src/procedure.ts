feeb2e799fe6018ed728dc10b1a561b64b20b8dd
yangcurve <utsu0621@gmail.com>
2024-10-19 09:45:44 +0900

    .

--- src/procedure.ts
+++ src/procedure.ts
@@ -2,7 +2,7 @@ import { type Action, type ActionType } from './action'
 import { z } from 'zod'
 
 type ActionBuilder<Type extends ActionType, Schema extends z.ZodType> = <Output>(
-  resolver: (input: z.infer<Schema>) => Output,
+  resolver: (input: z.infer<Schema>) => Output | Promise<Output>,
 ) => Action<Type, z.input<Schema>, Output>
 
 type ActionBuilderWithoutInput<Type extends ActionType> = ActionBuilder<Type, z.ZodVoid>
@@ -18,7 +18,7 @@ const getActionBuilder =
   <T extends ActionType, Schema extends z.ZodTypeAny>(Schema: Schema): ActionBuilder<T, Schema> =>
   (resolver) =>
   async (input) =>
-    await resolver(Schema.parse(input))
+    resolver(Schema.parse(input))
 
 type Procedure = {
   query: ActionBuilderWithoutInput<'query'>
