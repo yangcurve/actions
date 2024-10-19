7d18e55db844d3c3481063263f698ec403b90ff5
yangcurve <utsu0621@gmail.com>
2024-10-20 05:27:38 +0900

    middleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './action'
 export * from './client'
+export * from './procedure'
 export * from './utils'
---
+++ src/procedure.ts
@@ -0,0 +1,47 @@
+import { type Action, type ActionType } from './action'
+import { z } from 'zod'
+
+type Resolver<Context, Input, Output> = (param: { ctx: Context; input: Input }) => Promise<Output>
+
+type ActionBuilder<Type extends ActionType, Context, Schema extends z.ZodType> = <Output>(
+  resolver: Resolver<Context, z.infer<Schema>, Output>,
+) => Action<Type, z.input<Schema>, Output>
+
+type Middleware<Context, NewContext> = (ctx: Context) => NewContext | Promise<NewContext>
+
+type Procedure<Context> = {
+  use: <NewContext extends Context>(middleware: Middleware<Context, NewContext>) => Procedure<NewContext>
+  query: ActionBuilder<'query', Context, z.ZodVoid>
+  mutation: ActionBuilder<'mutation', Context, z.ZodVoid>
+  input: <Schema extends z.ZodType, Type extends ActionType>(
+    Schema: Schema,
+  ) => {
+    query: ActionBuilder<Type, Context, Schema>
+    mutation: ActionBuilder<Type, Context, Schema>
+  }
+}
+
+type ProcedureBuilder = <Context>(createContext: () => Context | Promise<Context>) => Procedure<Context>
+
+export const createProcedure: ProcedureBuilder = (createContext = () => new Promise(() => {})) => {
+  type Context = Awaited<ReturnType<typeof createContext>>
+
+  const getActionBuilder =
+    <Schema extends z.ZodType, T extends ActionType>(Schema: Schema): ActionBuilder<T, Context, Schema> =>
+    (resolver) =>
+    async (input) =>
+      await resolver({
+        ctx: await createContext(),
+        input: Schema.parse(input) as z.infer<typeof Schema>,
+      })
+
+  return {
+    use: (middleware) => createProcedure(async () => await middleware(await createContext())),
+    query: getActionBuilder(z.void()),
+    mutation: getActionBuilder(z.void()),
+    input: (Schema) => ({
+      query: getActionBuilder(Schema),
+      mutation: getActionBuilder(Schema),
+    }),
+  }
+}
